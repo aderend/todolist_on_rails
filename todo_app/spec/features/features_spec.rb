48be30887a6b5c0b429e7f6bbf0e522c441bc1e9
Deren Dogrusoy & Leah Goldberg <pair+aderend.leahgoldberg@devbootcamp.com>
2015-09-07 12:00:05 -0400

    add test to check if clicking on list name redirects to tasks

--- todo_app/spec/features/features_spec.rb
+++ todo_app/spec/features/features_spec.rb
@@ -3,11 +3,19 @@ require 'rails_helper'
 feature 'list management' do
 
   let!(:sample_list) {List.create!(category: Faker::Lorem.word, created_by: Faker::Name.name)}
+  let!(:sample_task) {Task.create!(name: Faker::Lorem.word, list_id: sample_list.id)}
 
   scenario "user can see list of lists on index page" do
     visit "/"
-    save_and_open_page
     expect(page).to have_content(sample_list.category.capitalize)
+  end
+
+  scenario "clicking on list name takes you list page" do
+    visit "/"
+    click_link(sample_list.category.capitalize)
+    save_and_open_page
+    expect(page).to have_content(sample_list.category)
+    expect(page).to have_content(sample_task.name)
   end
 
 end
