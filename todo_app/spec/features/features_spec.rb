d509286377051e32298636fce8159a22831c7cc9
Deren Dogrusoy & Leah Goldberg <pair+aderend.leahgoldberg@devbootcamp.com>
2015-09-07 17:28:56 -0400

    add database cleaner gem

--- todo_app/spec/features/features_spec.rb
+++ todo_app/spec/features/features_spec.rb
@@ -5,6 +5,13 @@ feature 'list management' do
   let!(:sample_list) {List.create!(category: Faker::Lorem.word, created_by: Faker::Name.name)}
   let!(:sample_task) {Task.create!(name: Faker::Lorem.word, list_id: sample_list.id)}
 
+  def create_new_list
+    @new_category = Faker::Lorem.word
+    @new_created_by = Faker::Name.name
+    fill_in("list_category", with: @new_category)
+    fill_in("list_created_by", with: @new_created_by)
+  end
+
   scenario "user can see list of lists on index page" do
     visit "/"
     expect(page).to have_content(sample_list.category)
@@ -23,13 +30,10 @@ feature 'list management' do
   scenario "editing a list saves successfully" do
     visit "/"
     click_button('Edit')
-    new_category = Faker::Lorem.word
-    new_created_by = Faker::Name.name
-    fill_in("list_category", with: new_category)
-    fill_in("list_created_by", with: new_created_by)
+    create_new_list
     click_button('Update List')
-    expect(page).to have_content(new_category)
-    expect(page).to have_content(new_created_by)
+    expect(page).to have_content(@new_category)
+    expect(page).to have_content(@new_created_by)
   end
 
   scenario "each list has a delete button that removes the list" do
@@ -42,13 +46,10 @@ feature 'list management' do
   scenario "creating a new list saves and redirects to index page" do
     visit "/"
     click_button('New List')
-    new_category = Faker::Lorem.word
-    new_created_by = Faker::Name.name
-    fill_in("list_category", with: new_category)
-    fill_in("list_created_by", with: new_created_by)
+    create_new_list
     click_button("Create")
-    expect(page).to have_content(new_category)
-    expect(page).to have_content(new_created_by)
+    expect(page).to have_content(@new_category)
+    expect(page).to have_content(@new_created_by)
   end
 
   scenario "clicking on list name takes you list page" do
