ad373ba561aff095617e243906b2f84f1dc84474
Deren Dogrusoy & Leah Goldberg <pair+aderend.leahgoldberg@devbootcamp.com>
2015-09-07 16:22:31 -0400

    rspwc for create, and redirects

--- todo_app/spec/controllers/list_controller_spec.rb
+++ todo_app/spec/controllers/list_controller_spec.rb
@@ -1,7 +1,11 @@
 require 'rails_helper'
 
   describe ListsController do
-    FactoryGirl.lint
+    factories_to_lint = FactoryGirl.factories.reject do |factory|
+      factory.name =~ /^invalid_/
+    end
+
+    FactoryGirl.lint(factories_to_lint)
 
     describe '#index' do
       it 'shows all the lists' do
@@ -29,12 +33,27 @@ require 'rails_helper'
 
     describe '#create' do
       it 'creates a new list object' do
-        list_attributes = FactoryGirl.attributes_for(:list)
+        list = FactoryGirl.create(:list)
+        list_attributes = list.attributes
         post :create, list: list_attributes
-        expect(List.last.attri)
-
-
+        expect(List.last.category).to eq(list.category)
+        expect(List.last.created_by).to eq(list.created_by)
       end
+
+      it 'redirects to route after creating' do
+        list = FactoryGirl.create(:list)
+        list_attributes = list.attributes
+        post :create, list: list_attributes
+        expect(response).to redirect_to lists_path
+      end
+
+      it 'reloads the page after failing to create' do
+        # list = FactoryGirl.create(:invalid_list)
+        list_attributes = FactoryGirl.attributes_for(:invalid_list)
+        post :create, list: list_attributes
+        expect(response).to redirect_to new_list_path
+      end
+
     end
 
 
--- todo_app/spec/factories/factories.rb
+++ todo_app/spec/factories/factories.rb
@@ -1,10 +1,13 @@
-require 'rails_helper'
+# require 'rails_helper'
 
 FactoryGirl.define do
 
   factory(:list) do
     category Faker::Lorem.word
     created_by Faker::Name.name
+    factory(:invalid_list) do
+      category nil
+    end
   end
 
   factory(:task) do
